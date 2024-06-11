d4974a8dada758bfddfa6b5d4b4545d197d93b26
sheryl acree <sacree@tql.com>
2024-06-11 14:52:07 -0400

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static string Name { get; set; } = "Sheryl";
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Hello, {Name} World!");
+                  }
+    }
+}
